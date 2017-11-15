a719c9c8ad264a6e874fac960dff9872527b1ee1
imquanquan <imquanquan99@gmail.com>
2017-11-15 08:56:26 +0800

    change subject list to Chinese

--- subject_rank/subject_rank.py
+++ subject_rank/subject_rank.py
@@ -11,6 +11,27 @@ base_url = "http://www.zuihaodaxue.com/BCSR/"
 RankData_re = re.compile(r'^(\d+)(前\d+%)(.*?)(\d+)$')
 PhD_re = re.compile(r'.级学科博士学位授权点')
 core_subject_re = re.compile(r'.级学科国家重点学科')
+arr_subject_en =   [
+                        [ "哲学" ],
+                        [ "理论经济学", "应用经济学" ],
+                        [ "法学", "政治学", "社会学", "民族学", "马克思主义理论" ],
+                        [ "教育学", "心理学", "体育学" ],
+                        [ "中国语言文学", "外国语言文学", "新闻传播学" ],
+                        [ "考古学", "中国史", "世界史" ],
+                        [ "数学", "物理学", "化学", "天文学", "地理学", "大气科学", "海洋科学", "地球物理学", "地质学",
+                          "生物学", "生态学", "统计学" ],
+                        [ "力学", "机械工程", "光学工程", "仪器科学与技术", "材料科学与工程", "冶金工程", "动力工程及工程热物理",
+                          "电气工程", "电子科学与技术", "信息与通信工程", "控制科学与工程", "计算机科学与技术", "建筑学",
+                                        "土木工程", "水利工程", "测绘科学与技术", "化学工程与技术", "地质资源与地质工程", "矿业工程",
+                                        "石油与天然气工程", "纺织科学与工程", "轻工技术与工程", "交通运输工程", "船舶与海洋工程",
+                                        "航空宇航科学与技术", "核科学与技术", "农业工程", "环境科学与工程", "生物医学工程",
+                                        "食品科学与工程", "城乡规划学", "软件工程", "安全科学与工程" ],
+                        [ "作物学", "园艺学", "农业资源与环境", "植物保护", "畜牧学", "兽医学", "林学", "水产", "草学" ],
+                        [ "基础医学", "临床医学", "口腔医学", "公共卫生与预防医学", "中医学", "中西医结合", "药学", "中药学",
+                          "特种医学", "护理学" ],
+                        [ "管理科学与工程", "工商管理", "农林经济管理", "公共管理", "图书情报与档案管理" ],
+                        [ "艺术学理论", "音乐与舞蹈学", "戏剧与影视学", "美术学", "设计学" ], ]
+
 
 arr_subject_link = [
 			[ "zhexue2017" ],
@@ -33,13 +54,16 @@ arr_subject_link = [
 			[ "guanlikexueyugongcheng2017", "gongshangguanli2017", "nonglinjingjiguanli2017", "gonggongguanli2017", "tushuqingbaoyudanganguanli2017" ],
 			[ "yishuxuelilun2017", "yinyueyuwudaoxue2017", "xijuyuyingshixue2017", "meishuxue2017", "shejixue2017" ], ]
 
+arr_subject_list = sum(arr_subject_en, [])
+arr_subject_link_list = sum(arr_subject_link, [])
+
 i = 0
-for arr_subject in sum(arr_subject_link, []):
+for arr_subject in arr_subject_link_list:
     url = base_url + arr_subject + '.html'
     html_source = requests.get(url).content.decode('utf-8')
     soup = BeautifulSoup(html_source, 'html.parser')
     for rank_data in soup.find_all('tr', class_='bgfd'):
-        subject = arr_subject[:-4]
+        subject = arr_subject_list[arr_subject_link_list.index(arr_subject)]
         rank, percentile, university_name, score = RankData_re.findall(rank_data.text)[0]
         if PhD_re.findall(str(rank_data)):
             PhD = PhD_re.findall(str(rank_data))[0]
